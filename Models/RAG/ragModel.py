334f148423253b027900089b86d074bc9cd44bf3
atlno1fan <alltimelowno.1fan@gmail.com>
2023-08-27 03:07:32 +0300

    Added paragraph selection function and chatgpt

--- Models/RAG/ragModel.py
+++ Models/RAG/ragModel.py
@@ -40,6 +40,61 @@ class RAG_target_paragraph_generator:
 
         return text, self.link
 
+    def get_paragraphs_RAG(self, text):
+        self.question, link = self.check_link_in_text(text)
+        if self.link != link:
+            self.paragraphs, self.titles = pdf_to_html(link)
+
+            (
+                self.chunked_corpus,
+                self.passage_titles,
+                self.passages,
+            ) = paragraph_splitter(self.paragraphs, self.titles)
+
+            self.input_ids = tokenize_paragraph(self.chunked_corpus)
+
+            self.ctx_encoder, self.device = set_device()
+
+            self.embeddings = generate_embeddings(
+                self.input_ids, self.ctx_encoder, self.device
+            )
+
+            self.index, self.dim, self.m = add_faiss_index(self.embeddings)
+
+            self.dataset = generate_dataset(
+                self.chunked_corpus,
+                self.embeddings,
+                self.index,
+                self.dim,
+                self.m,
+                self.passage_titles,
+                self.passages,
+            )
+
+            self.tokenizer, self.model = self.intialize_model(self.dataset)
+            self.link = link
+            # print(link)
+        # else:
+        #    print('link is the same')
+
+        input_ids = self.tokenizer(self.question, return_tensors="pt").input_ids.to(
+            self.device
+        )
+
+        # Get question embeddings
+        question_embeddings = self.model.question_encoder(input_ids)[0]
+
+        # Convert to NumPy
+        question_embeddings = question_embeddings.detach().cpu().numpy()
+
+        docs = self.model.retriever.retrieve(question_embeddings, n_docs=5)
+
+        generated = self.model.generate(input_ids=input_ids)
+
+        encoded, doc_ids, doc_texts = docs
+        target_paragraph = "\n".join(doc_texts[0]["text"])
+        return target_paragraph
+    
     def prompt_generator(self, text):
         target_paragraph = self.get_paragraphs_RAG(text)
         prompt = (
@@ -50,3 +105,5 @@ class RAG_target_paragraph_generator:
             + " '''"
         )
         return prompt
+    
+    
---
+++ main.py
@@ -0,0 +1,42 @@
+import openai
+from Models.RAG.RagModel import *
+from Models.RAG.RagModel import RAG_target_paragraph_generator
+import argparse
+import apiKeys
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=int, default="RAG")
+    args = parser.parse_args()
+
+    if args.model == "RAG":
+        generator = RAG_target_paragraph_generator("", "")
+
+    openai.api_key = apiKeys.openai_key
+    # If memory matters
+    # Initialize conversation history
+    conversation_history = []
+    counter = 0
+    while True:
+        # Get user input
+        user_input = input("You: ")
+
+        if user_input.lower() == "exit":
+            break
+
+        # Add user message to conversation history
+        conversation_history.append(
+            {"role": "user", "content": generator.prompt_generator(user_input)}
+        )
+
+        # Make API call
+        api_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=conversation_history
+        )
+
+        # Extract and print assistant's reply
+        assistant_reply = api_response.choices[0].message["content"]
+        print("Assistant:", assistant_reply)
+
+        # Add assistant's reply to conversation history
+        conversation_history.append({"role": "assistant", "content": assistant_reply})
